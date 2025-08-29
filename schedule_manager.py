7e0a082851e996ef8fcdb7454a65b0514d581132
이준혁 <you@example.com>
2025-08-29 21:58:56 +0900

    first

--- schedule_manager.py
+++ schedule_manager.py
@@ -1,6 +1,8 @@
 import pymysql
 
 from pymysql.cursors import Cursor
+from win32comext.mapi.emsabtags import PR_EMS_AB_LOCAL_INITIAL_TURN
+
 
 # db 연결하는 함수
 def get_db_connection() -> pymysql.Connection:
@@ -11,16 +13,52 @@ def get_db_connection() -> pymysql.Connection:
         exit(1)
 
 def add_schedule(cursor: Cursor):
-    # Todo: 여기에 일정 추가 코드를 작성합니다.
-    raise NotImplementedError("함수 미구현")
+    #
+    title = input("제목: ")
+    desc = input("설명: ")
+    start_datetime = input("시작 시간(yyyymmddhhmmss)")
+    end_datetime = input("종료 시간(yyyymmddhhmmss)")
+
+    sql = """
+            INSERT INTO schedules (title, description, start_datetime, end_datetime)
+            VALUES (%s, %s, %s, %s)
+    """
+
+    cursor.execute(sql, (title, desc, start_datetime, end_datetime))
+    print("일정 추가 됨")
 
 def get_schedules(cursor: Cursor):
-    # Todo: 여기에 일정 정보를 가져오는 코드를 작성합니다.
-    raise NotImplementedError("함수 미구현")
+    sql = "SELECT id, title, description, start_datetime, is_completed FROM schedules"
+
+    cursor.execute(sql)
+    #쿼리에서 실행된 튜플 가져오기
+    schedules = cursor.fetchall()
+    if not schedules:
+        print("등록된 일정이 없습니다")
+        return
+
+    #가져온 정보 출력
+    for schedule in schedules:
+        id, title, description, start_datetime, is_completed = schedule
+        status = "완료" if is_completed else "미완료"
+
+        print(f"[{id}] {title}")
+        print(f"   상태: {status}")
+        print(f"   시간: {start_datetime}")
+        if description:
+            print(f"    설명: {description}")
+        print()
 
 def complete_schedule(cursor: Cursor):
-    # Todo: 여기에 일정을 완료처리하는 코드를 작성합니다.
-    raise NotImplementedError("함수 미구현")
+    #완료 시킬 일정의 id값을 입력 받는다.
+    id = input("아이디 값:")
+
+    #update 쿼리 작성
+    sql = "UPDATE schedules SET is_completed = true where id = %s"
+
+    #update 쿼리 실행
+    cursor.execute(sql, id)
+    print("일정을 완료함.")
 
 # 메뉴 나타내기
 def show_menu() -> str:
